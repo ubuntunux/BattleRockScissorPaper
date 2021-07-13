b44eafa020a80445895f8c5b3d936346752a2b8b
Ubuntunux <ubuntunux@gmail.com>
2021-07-14 03:18:47 +0900

    update

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -10,7 +10,7 @@ public class Constants : MonoBehaviour
     static public float GroundPosition = 3.0f;
     static public float AttackDistance = 1.5f;
     static public float IdleDistance = 2.0f; 
-    static public int InitialHP = 3;
+    static public int InitialHP = 5;
     static public int DamageDraw = 1;
     static public int DamageLoose = 2;
 }
